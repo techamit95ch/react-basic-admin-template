9bee937495a4079c1df9daa53db07b2772c9116e
techamit95ch <amit.hellmaker@gmail.com>
2021-06-15 00:59:20 +0530

    Topbar just made

--- src/componenets/topbar/TopBar.jsx
+++ src/componenets/topbar/TopBar.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import "./Toppbar.css";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
 export default function TopBar() {
   return (
-  <div className={"topbar"}>
+    <div className={"topbar"}>
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo"></span>
-          
+          <span className="logo"> Notes Dist </span>{" "}
         </div>
-        <div className="topRight">right</div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topbarIconBag">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topbarIconBag">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="https://cactusthemes.com/blog/wp-content/uploads/2018/01/tt_avatar_small.jpg"
+            className="topAvatar"
+            alt="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWAJ0Ge_MNL1cOKBqqX_2CwLWNjY_vAYgHjQ&usqp=CAU"
+          />
+          <div className="topbarsIcons"> </div>{" "}
+        </div>
       </div>
-  </div> 
-  )
+    </div>
+  );
 }
