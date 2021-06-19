51b3f751403fdce3782827b33e6489805c6bb319
techamit95ch <amit.hellmaker@gmail.com>
2021-06-19 15:12:21 +0530

    note create is ready

--- src/pages/notes/notesCreate/NotesCreate.jsx
+++ src/pages/notes/notesCreate/NotesCreate.jsx
@@ -4,27 +4,59 @@ import { Link } from "react-router-dom";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import {
+  DefaultView,
+  ImgView,
+  PdfView,
+  DocView,
+  EditorView,
+  MediaView,
+} from "./form/Form";
 import { Avatar, Button, Menu, MenuItem, Fade } from "@material-ui/core";
-import Nav from "react-bootstrap/Nav";
-
-import NavDropdown from "react-bootstrap/NavDropdown";
+import {
+  NavDropdown,
+  TabPane,
+  TabContent,
+  TabContainer,
+  Tab,
+  Tabs,
+  Nav,
+} from "react-bootstrap";
 export default function NotesCreate() {
-  const [getForm, setGetForm] = useState("text");
+  const [key, setKey] = useState("home");
   return (
     <Container className="notescreate">
       <Row>
         <Col sm={12}>
-          <Nav justify variant="tabs" defaultActiveKey="#">
-            <Nav.Item>
-              <Nav.Link href="#">Active</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="link-2">Link</Nav.Link>
-            </Nav.Item>
-          </Nav>
+          <Tabs
+            id="controlled-tab-example"
+            activeKey={key}
+            onSelect={(k) => setKey(k)}
+          >
+            <Tab
+              eventKey="chooseNote"
+              title="Choose Note"
+              disabled
+              activeKey={false}
+            >
+              <DefaultView />
+            </Tab>
+            <Tab eventKey="editor" title="Editor">
+              <EditorView />
+            </Tab>
+            <Tab eventKey="image" title="Image">
+              <ImgView />
+            </Tab>
+            <Tab eventKey="doc" title="Doc">
+              <DocView />
+            </Tab>
+            <Tab eventKey="pdf" title="Pdf">
+              <PdfView />
+            </Tab>
+            <Tab eventKey="media" title="Media">
+              <MediaView />
+            </Tab>
+          </Tabs>
         </Col>
       </Row>
     </Container>
--- src/pages/notes/notesCreate/form/Form.jsx
+++ src/pages/notes/notesCreate/form/Form.jsx
@@ -1,18 +1,24 @@
 import React, { useState, Component } from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-
-import { Avatar, Button } from "@material-ui/core";
+import { Avatar, Button,Fab } from "@material-ui/core";
 import { GetAppRounded, Close } from "@material-ui/icons";
-import { Col, Row, Container } from "react-bootstrap";
+import {
+  Col,
+  Row,
+  Container,
+  Form,
+  FormControl,
+  FormCheck,
+  FloatingLabel,
+} from "react-bootstrap";
 import "./style.css";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-// import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import DecoupledEditor from "@ckeditor/ckeditor5-build-decoupled-document";
+import FileBase from "react-file-base64";
+import { useSelector, useDispatch } from "react-redux";
 
 let isDirty = false;
-
-// import PDF from "./Resume_1.pdf";
 const useStyles = makeStyles((theme) => ({
   avatarImg: {
     position: "relative",
@@ -20,58 +26,181 @@ const useStyles = makeStyles((theme) => ({
     margin: "0",
   },
 }));
-
-export const VDialogImg = (props) => {
-  const classes = useStyles();
-  return <img className={classes.avatarImg} src={props.path} />;
-};
-export const VDialogPDF = (props) => {
+export const DefaultView = (props) => {
   const classes = useStyles();
   return (
-    <iframe
-      src={props.path}
-      type="application/pdf"
-      className={classes.avatarImg}
-    ></iframe>
+    <Container>
+      <Row>
+        <Col></Col>
+      </Row>
+    </Container>
   );
 };
-export class VDialogText extends Component {
-  // const classes = useStyles();
+export const ImgView = (props) => {
+  const classes = useStyles();
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Form>
+  <Form.Group controlId="formGroupEmail">
+    <Form.Label>Upload Image</Form.Label>
+    <div className={classes.fileInput}>
+            <FileBase
+              type="file"
+              multiple={false}
+              name="selectedFile"
+              onDone={
+                ""
+                // ({ base64 }) =>
+                // setPostData({ ...postData, selectedFile: base64 })
+              }
+            />
+          </div>
+  </Form.Group>
+  <Form.Group controlId="formGroupPassword">
 
+    <Fab variant="extended" color="success" aria-label="add" type="submit">Submit</Fab>
+  </Form.Group>
+</Form>
+
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export const PdfView = (props) => {
+  const classes = useStyles();
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Form>
+  <Form.Group controlId="formGroupEmail">
+    <Form.Label>Upload PDF</Form.Label>
+    <div className={classes.fileInput}>
+            <FileBase
+              type="file"
+              multiple={false}
+              name="selectedFile"
+              onDone={
+                ""
+                // ({ base64 }) =>
+                // setPostData({ ...postData, selectedFile: base64 })
+              }
+            />
+          </div>
+  </Form.Group>
+  <Form.Group controlId="formGroupPassword">
+
+    <Fab variant="extended" color="success" aria-label="add" type="submit">Submit</Fab>
+  </Form.Group>
+</Form>
+
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export const DocView = (props) => {
+  const classes = useStyles();
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Form>
+  <Form.Group controlId="formGroupEmail">
+    <Form.Label>Upload Doc</Form.Label>
+    <div className={classes.fileInput}>
+            <FileBase
+              type="file"
+              multiple={false}
+              name="selectedFile"
+              onDone={
+                ""
+                // ({ base64 }) =>
+                // setPostData({ ...postData, selectedFile: base64 })
+              }
+            />
+          </div>
+  </Form.Group>
+  <Form.Group controlId="formGroupButton">
+
+    <Fab variant="extended" color="success" aria-label="add" type="submit">Submit</Fab>
+  </Form.Group>
+  </Form>
+
+        </Col>
+      </Row>
+    </Container>
+  );
+  };
+export const MediaView = (props) => {
+  const classes = useStyles();
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Form>
+            <Form.Label>Email address</Form.Label>
+          <Form.Control type="link" placeholder=" video Link" />
+              <Form.Text className="text-muted">
+                Provide exact video url from iframe src of the video
+              </Form.Text>
+  <Form.Group controlId="formGroupPassword">
+
+    <Fab variant="extended" color="success" aria-label="add" type="submit">Submit</Fab>
+  </Form.Group>
+  </Form>
+
+        </Col>
+      </Row>
+    </Container>
+  );
+  };
+
+export class EditorView extends Component {
+  // const classes = useStyles();
+  formData = null;
   editor = null;
+  handleSubmit=(event)=>{
+    event.preventDefault();
+    const data = this.formData;
+    console.log(data);
+  }
   render() {
     return (
-      <CKEditor
-        className="avatarImg"
-        onReady={(editor) => {
-          console.log("Editor is ready to use!", editor);
+      <Container>
+        <Form onSubmit={this.handleSubmit}><Col sm={12}>
+        <CKEditor className="form-control"
 
-          // Insert the toolbar before the editable area.
-          editor.ui
-            .getEditableElement()
-            .parentElement.insertBefore(
-              editor.ui.view.toolbar.element,
-              editor.ui.getEditableElement()
-            );
-
-          this.editor = editor;
-        }}
-        onError={({ willEditorRestart }) => {
-          // If the editor is restarted, the toolbar element will be created once again.
-          // The `onReady` callback will be called again and the new toolbar will be added.
-          // This is why you need to remove the older toolbar.
-          if (willEditorRestart) {
-            this.editor.ui.view.toolbar.element.remove();
-          }
-        }}
-        onChange={(event, editor) => console.log({ event, editor })}
-        editor={DecoupledEditor}
-        data="<p>Hello from CKEditor 5's decoupled editor!</p>"
-      />
+          onReady={(editor) => {
+            editor.ui
+              .getEditableElement()
+              .parentElement.insertBefore(
+                editor.ui.view.toolbar.element,
+                editor.ui.getEditableElement()
+              );
+            this.editor = editor;
+          }}
+          onError={({ willEditorRestart }) => {
+            if (willEditorRestart) {
+              this.editor.ui.view.toolbar.element.remove();
+            }
+          }}
+          onChange={(event, editor) => {
+             const data = editor.getData();
+             this.formData= data;
+          }}
+          editor={DecoupledEditor}
+          data=""
+        /></Col><Row><Col sm={12}>
+      <Col sm={9}></Col>
+    <Col sm={3} float="right">
+      <Fab variant="extended" color="primary" aria-label="add" type="submit">Submit</Fab>
+  </Col></Col></Row>
+  </Form>
+      </Container>
     );
   }
 }
-export const VDialogWord = (props) => {
-  const classes = useStyles();
-  return <iframe src={props.path} className={classes.avatarImg}></iframe>;
-};
